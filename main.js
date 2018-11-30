03d451eecdda2cfa8a2d6e2dc5f648a47746cb8a
* <webstermath@gmail.com>
2018-11-29 21:31:40 -0800

    added workbox and sw

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 import {makePlayerApp} from './app.js';
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//       // Registration was successful
-//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//     }, function(err) {
-//       // registration failed :(
-//       console.log('ServiceWorker registration failed: ', err);
-//     });
-//   });
-// }
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
   function ready(eventHandler){
     if (document.readyState !== 'loading') {
--- sw.js
+++ sw.js
@@ -1,10 +1,31 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  new RegExp('.*'),
+  workbox.strategies.networkFirst()
+);
+
+
+
 self.addEventListener('install', function(event) {
   console.log('sw install',event)
 });
 
+self.addEventListener('activate', function(event) {
+  console.log('sw activate',event)
+
+
+});
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request)
+  //console.log(event.request)
   // event.respondWith(
   //   caches.match(event.request)
   //     .then(function(response) {
