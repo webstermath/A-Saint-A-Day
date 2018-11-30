f7fd48f00ecb8d1ad5bf960800ef16db6dc65ec7
* <webstermath@gmail.com>
2018-11-30 07:17:58 -0800

    more strategies

--- sw.js
+++ sw.js
@@ -6,35 +6,115 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+//new RegExp('8000\/$')
+//new RegExp('\/')http://127.0.0.1:8000/?type=today&dateKey=2018-11-29&track=0
+//new RegExp('8000\/?\?')
+//new RegExp('8000\/?\??[^.]*$')
+//workbox.strategies.networkFirst()
+
+//workbox.strategies.cacheFirst
+//workbox.strategies.staleWhileRevalidate()
+
+
+/*
+workbox.routing.registerRoute(
+  new RegExp('/images/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Only cache requests for a week
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+        // Only cache 10 requests.
+        maxEntries: 10,
+      }),
+    ]
+  })
+);
+*/
+workbox.routing.registerRoute(
+  matchGeneral,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'general-cache-v1'
+    })
+);
+function matchGeneral(request){
+ const pathname = request.url.pathname;
+ return ['mp3','woff2','png'].every( ext => !pathname.endsWith(ext));
+}
+
 
 workbox.routing.registerRoute(
-  new RegExp('.*'),
-  workbox.strategies.networkFirst()
+  matchImages,
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache-v1',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Only cache requests for a week
+        maxAgeSeconds: 7 * 24 * 60 * 60
+        // Only cache 10 requests.
+        //maxEntries: 10,
+      }),
+    ]
+  })
 );
+function matchImages(request){
+const pathname = request.url.pathname;
+return ['png'].some( ext => pathname.endsWith(ext));
+}
+
+workbox.routing.registerRoute(
+  matchFonts,
+  workbox.strategies.cacheFirst({
+    cacheName: 'fonts-cache-v1',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Only cache requests for a week
+        maxAgeSeconds: 7 * 24 * 60 * 60
+        // Only cache 10 requests.
+        //maxEntries: 10,
+      }),
+    ]
+  })
+);
+function matchFonts(request){
+const pathname = request.url.pathname;
+return ['woff2'].some( ext => pathname.endsWith(ext));
+}
+
+workbox.routing.registerRoute(
+  matchAudio,
+  workbox.strategies.cacheFirst({
+    cacheName: 'audio-cache-v1',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Only cache requests for a week
+        maxAgeSeconds: 31 * 24 * 60 * 60
+        // Only cache 10 requests.
+        //maxEntries: 10,
+      }),
+    ]
+  })
+);
+function matchAudio(request){
+const pathname = request.url.pathname;
+return ['mp3'].some( ext => pathname.endsWith(ext));
+}
 
 
 
-self.addEventListener('install', function(event) {
-  console.log('sw install',event)
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('sw activate',event)
 
 
-});
+// self.addEventListener('install', function(event) {
+//   console.log('sw install',event)
+// });
 
-self.addEventListener('fetch', function(event) {
-  //console.log(event.request)
-  // event.respondWith(
-  //   caches.match(event.request)
-  //     .then(function(response) {
-  //       // Cache hit - return response
-  //       if (response) {
-  //         return response;
-  //       }
-  //       return fetch(event.request);
-  //     }
-  //   )
-  // );
-});
+// self.addEventListener('activate', function(event) {
+//   console.log('sw activate',event)
+
+
+// });
+
+// self.addEventListener('fetch', function(event) {
+
+// });
